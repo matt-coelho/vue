8152afe00de669fdf6be0137569db84b9a1bb415
matt-coelho <matheus.coelho@outlook.com>
2023-02-12 12:04:05 -0300

    props and custom events provide and inject global and local components scoped styles slots

---
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-card>
+    <div>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </div>
+    <p>{{ infoText }}</p>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: ["fullName", "infoText", "role"],
+}
+</script>
+
+<style scoped>
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
